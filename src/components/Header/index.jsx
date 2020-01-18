0cb9df45425cf4b3f533f9a779be9412fc87e792
unknown <harryfreyr@gmail.com>
2020-01-18 11:53:16 +0000

    remove space

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,7 +10,7 @@ export default (props) => {
         <span className="slider round"></span>
       </label>
       <h1>Hello !</h1>
-      <h2>My name is <span className='name'>Alexander Freyr Sveinsson</span> I am a software engineer focusing on creating beautiful and  intuitive tools</h2>
+      <h2>My name is <span className='name'>Alexander Freyr Sveinsson</span> I am a software engineer focusing on creating beautiful and intuitive tools</h2>
       <h3>Get in touch <a href='mailto:alexfreyrs@gmail.com'>alexfreyrs@gmail.com</a></h3>
     </header>
   );
