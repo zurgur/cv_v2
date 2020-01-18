5b7d904fed43591234a481cc0a106d9e2e4e5703
unknown <harryfreyr@gmail.com>
2020-01-18 12:02:06 +0000

    Updates

---
+++ precache-manifest.6030bc4d8cb1ee0018ba0693118fc6d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30dc76069628cb22b939d4231e8ef41b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c0fd88a595e67627f24",
+    "url": "./static/css/main.6ccc94a5.chunk.css"
+  },
+  {
+    "revision": "1945c778a8ece81644c1",
+    "url": "./static/js/2.5785b3d1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.5785b3d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c0fd88a595e67627f24",
+    "url": "./static/js/main.912c593a.chunk.js"
+  },
+  {
+    "revision": "07aab79528753131009e",
+    "url": "./static/js/runtime-main.71b61088.js"
+  }
+]);
